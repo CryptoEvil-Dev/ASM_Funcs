d4585c4d34d898953c43df31bb85daa96ec7cf01
SHIRO <58366293+CryptoEvil-Dev@users.noreply.github.com>
2023-11-12 17:16:26 +0300

    Update ASMout.c

--- ASMout.c
+++ ASMout.c
@@ -1,8 +1,12 @@
-void ASMout(char *message);
-
+#include <stdio.h>
+void NASMWrite(char *message);
+int NASMSize(char *text);
 int main(int argc, char *argv[]){
 
-    ASMout("Hello ASMout");
+    char message[] = "Hello NASM\nlen ";
+    NASMWrite(message);
+    int size = NASMSize(message);
 
+    printf("%d\n", size);
     return 0;
 }
